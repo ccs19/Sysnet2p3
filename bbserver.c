b49685d997b7a705c310ebf16882aaa9d26fa8b2
Brett Rowberry <brett.rowberry@gmail.com>
2015-02-20 18:03:25 -0600

    Started server files.

---
+++ bbserver.c
@@ -0,0 +1,19 @@
+/*
+ * bbserver.c
+ * Systems and Networks II
+ * Project 3
+ * Christopher Schneider & Brett Rowberry
+ *
+ */
+
+int main(int argc, int ** argv)
+{
+    //get number of hosts wanted
+    //for number make hosts
+    //dynamic array of clientVar structs
+    //for number recevie messages and stick in array
+    //tell neighbors
+}
+
+//struct receive - in_addr, int port
+//struct send - toIP, to port, fromIP, fromPort, int hasToken
---
+++ bbserver.h
@@ -0,0 +1,8 @@
+/*
+ * bbserver.h
+ * Systems and Networks II
+ * Project 3
+ * Christopher Schneider & Brett Rowberry
+ *
+ */
+
