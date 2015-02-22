4201d8efa099271d28b947db006281b32f3e8d7f
Brett Rowberry <brett.rowberry@gmail.com>
2015-02-21 18:30:25 -0600

    Added pseudocode back to server.c

--- bbserver/bbserver.c
+++ bbserver/bbserver.c
@@ -11,3 +11,12 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+//get number of hosts wanted
+//for number make hosts
+//dynamic array of clientVar structs
+//for number recevie messages and stick in array
+//tell neighbors
+
+//struct receive - in_addr, int port
+//struct send - toIP, to port, fromIP, fromPort, int hasToken
