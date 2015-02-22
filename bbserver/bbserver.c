32cae8581bd3e82bc9c8761fca28686067e0332b
Brett Rowberry <brett.rowberry@gmail.com>
2015-02-21 19:41:58 -0600

    Host argument handling added to server.

--- bbserver/bbserver.c
+++ bbserver/bbserver.c
@@ -6,17 +6,49 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int numberOfHosts = -1;
+
+#define MIN_HOSTS 1
+#define MAX_HOSTS 10
+
+void printNumberOfHosts(int numberOfArgs, const char *inputString);
+
 int main(int argc, char** argv)
 {
+    printNumberOfHosts(argc, argv[1]); //get number of desired hosts
 
     return 0;
 }
 
-//get number of hosts wanted
-//for number make hosts
+void printNumberOfHosts(int numberOfArgs, const char *inputString)
+{
+    if(numberOfArgs != 2)
+    {
+        printf("Usage: ./bbserver <number of hosts>\n");
+        exit(1);
+    }
+
+    numberOfHosts = atoi(&inputString[0]);
+
+    if(numberOfHosts < MIN_HOSTS)
+    {
+        printf("%d host invalid. At least %d and at most %d hosts required.\n", numberOfHosts, MIN_HOSTS, MAX_HOSTS);
+    }
+    else if(numberOfHosts > MAX_HOSTS)
+    {
+        printf("%d hosts invalid. At least %d and at most %d hosts required.\n", numberOfHosts, MIN_HOSTS, MAX_HOSTS);
+    }
+    else
+    {
+        printf("Number of hosts: %d\n", numberOfHosts);
+    }
+}
+
 //dynamic array of clientVar structs
 //for number recevie messages and stick in array
 //tell neighbors
-
 //struct receive - in_addr, int port
 //struct send - toIP, to port, fromIP, fromPort, int hasToken
