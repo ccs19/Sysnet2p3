e27c9241d741b13fb3b01097cf214dca63f5124a
brett <br16@students.uwf.edu>
2015-03-08 18:07:46 -0500

    Cleanup.

--- common.c
+++ common.c
@@ -5,14 +5,6 @@
  * Christopher Schneider & Brett Rowberry
  */
 
-#include <netinet/in.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <netdb.h> //For gethostbyname()
-#include <arpa/inet.h>
-#include <unistd.h>
-
 #include "common.h"
 
 void PrintSockaddr_in(struct sockaddr_in *addr)
@@ -28,7 +20,7 @@ void PrintSockaddr_in(struct sockaddr_in *addr)
  */
 void PrintSendingInfo(SendingInfo *sendingInfo)
 {
-    printf("Neighbor info: ");
+    printf("\nNeighbor info: ");
     PrintSockaddr_in(&sendingInfo->neighborInfo);
     printf("Has token: %d\n", sendingInfo->hasToken);
     printf("Machine has exited: %d\n", sendingInfo->machineHasExited);
